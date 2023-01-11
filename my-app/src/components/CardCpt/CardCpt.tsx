ddd3c44cae4316ab6a93ee9104781b050167d928
Jsncndl <jsncndl1@gmail.com>
2023-01-11 21:09:38 +0100

    create new styled card named CardExp and CardExpList

---
+++ my-app/src/components/CardCpt/CardCpt.tsx
@@ -0,0 +1,52 @@
+import styled, { keyframes } from 'styled-components'
+import { FadeInX } from '../../styles/animations/animations'
+
+export interface CardCptProps {
+  title: string
+  icon?: string
+  indexDelay?: number
+}
+
+const animation = keyframes`
+  from {
+    transform: translateY(-100px);
+    opacity: 5%;
+    scale: 0.8;
+  }
+  65% {
+    transform: translateX(0);
+    opacity: 100%;
+  }
+  to {
+    scale: 1;
+  }
+`
+const StyledContainer = styled.li<{ indexDelay?: number | null }>`
+  height: 80px;
+  width: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  animation-name: ${animation};
+  animation-iteration-count: 1;
+  animation-fill-mode: both;
+  animation-duration: 400ms;
+  animation-delay: ${(props) =>
+    props.indexDelay !== undefined && props.indexDelay !== null && props.indexDelay !== 0
+      ? 300 + 100 * props.indexDelay
+      : 300}ms;
+`
+const StyledIcon = styled.img`
+  height: 50px;
+`
+
+export const CardCpt = ({ title, icon, indexDelay }: CardCptProps) => {
+  return (
+    <StyledContainer indexDelay={indexDelay}>
+      <FadeInX>
+        <StyledIcon src={icon} alt={'Logo de ' + title} />
+        <p>{title}</p>
+      </FadeInX>
+    </StyledContainer>
+  )
+}
---
+++ my-app/src/components/CardCptList/CardCptList.tsx
@@ -0,0 +1,42 @@
+import htmlIcon from '../../images/icon-html.svg'
+import cssIcon from '../../images/icon-css.svg'
+import sassIcon from '../../images/icon-sass.svg'
+import jsIcon from '../../images/icon-js.svg'
+import reactIcon from '../../images/icon-react.svg'
+import tsIcon from '../../images/icon-ts.svg'
+import nodeIcon from '../../images/icon-node-js.svg'
+import mongoIcon from '../../images/icon-mongodb.svg'
+import { CardCpt, CardCptProps } from '../CardCpt/CardCpt'
+import styled from 'styled-components'
+
+const competences: CardCptProps[] = [
+  { title: 'HTML', icon: htmlIcon },
+  { title: 'CSS', icon: cssIcon },
+  { title: 'Sass', icon: sassIcon },
+  { title: 'JavaScript', icon: jsIcon },
+  { title: 'React JS', icon: reactIcon },
+  { title: 'TypeScript', icon: tsIcon },
+  { title: 'MongoDB', icon: mongoIcon },
+  { title: 'Node.JS', icon: nodeIcon },
+]
+
+const StyledContainer = styled.div`
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding: 25px 15px;
+  gap: 20px;
+`
+
+
+export const CardCptList = () => {
+
+  return (
+    <StyledContainer>
+      {competences.map(({ title, icon }, index) => {
+        return <CardCpt key={index} title={title} icon={icon} indexDelay={index} />
+      })}
+    </StyledContainer>
+  )
+}
