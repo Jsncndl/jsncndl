289eaece07e4e73c199c9d744d7d8fdceda26ae1
Jsncndl <jsncndl1@gmail.com>
2023-01-18 01:58:06 +0100

    pages: add contact page contain contact form

---
+++ my-app/src/pages/Contact/Contact.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components'
+import { colors } from '../../styles/colors'
+
+const StyledForm = styled.form`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-self: center;
+  gap: 10px;
+`
+
+const StyledLabel = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+const StyledLabelName = styled.div`
+  padding: 5px 15px;
+`
+
+const StyledInput = styled.input<{ textarea?: boolean }>`
+  background-color: ${colors.third};
+  border-radius: 5px;
+  ${(props) => props.textarea && 'height: 20vh;'}
+`
+
+const StyledButton = styled.input`
+  width: 50%;
+  border: 0;
+  border-radius: 20px;
+  align-self: center;
+  height: 25px;
+  background-color: ${colors.third};
+  color: ${colors.white};
+  box-shadow: 0 0 5px ${colors.white};
+`
+
+export const Contact = () => {
+  return (
+    <span
+      style={{
+        alignSelf: 'center',
+        width: '90%',
+        height: '90vh',
+        display: 'flex',
+        flexDirection: 'column',
+      }}>
+      <h2>Me contacter</h2>
+      <StyledForm>
+        <StyledLabel>
+          <StyledLabelName>Prénom</StyledLabelName>
+          <StyledInput required type='text' />
+        </StyledLabel>
+        <StyledLabel>
+          <StyledLabelName>Nom</StyledLabelName>
+          <StyledInput required type='text' />
+        </StyledLabel>
+        <StyledLabel>
+          <StyledLabelName>Titre du message</StyledLabelName>
+          <StyledInput required type='text' />
+        </StyledLabel>
+        <StyledLabel>
+          <StyledLabelName>Message</StyledLabelName>
+          <StyledInput textarea required type='text' />
+        </StyledLabel>
+        <StyledButton type='submit' value='Envoyer' />
+      </StyledForm>
+    </span>
+  )
+}
